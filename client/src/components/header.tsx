f9eb4d4c9e225bca1d871b20b9716061587ddc5e
sumanth-08 <mrsumanth1947@gmail.com>
2025-06-21 11:50:32 +0530

    title and logo

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -2,7 +2,10 @@ const Header = () => {
   return (
     <nav className="sticky top-0 z-50 w-full bg-red-50 backdrop-blur py-2">
       <div className="container mx-auto flex h-12 items-center px-8">
-        <p className="font-semibold">Product Inventory Management</p>
+        <div className="flex gap-2 items-center">
+          <img src="/vite.svg" alt="" className="w-6 h-6" />
+          <p className="font-semibold">Product Inventory Management</p>
+        </div>
       </div>
     </nav>
   );
